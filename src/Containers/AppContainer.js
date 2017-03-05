5bd22cba118c3f1eb54a74e2f5df0c40720abe99
Harmony D <antancia@gmail.com>
2017-03-04 20:47:01 -0500

    First commit

---
+++ src/Containers/AppContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+
+    return (
+        <div>
+          <p>Hello World!</p>
+        </div>
+    )
+  }
+}
